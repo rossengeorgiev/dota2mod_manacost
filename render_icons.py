817865e8dddb7e23f15276d9274bf414f1bfa7d0
Rossen Georgiev <zoorty@gmail.com>
2014-04-12 20:02:05 +0100

    small rearrangements of the code

--- render_icons.py
+++ render_icons.py
@@ -5,12 +5,13 @@ import sys, os
 sys.path.append("./tools")
 import KeyValue
 
+# configs
 fill_blue = "#1f496f"
 fill_green = "#26721b"
 font = ImageFont.truetype('Exo-SemiBold.ttf', 13)
-src_image_path = "./src/items/"
-presrc_image_path = "./prerendered_items/"
-out_image_path = "./items/"
+src_images_root = "./src/items/"
+src_preimages_root = "./prerendered_items/"
+out_image_root = "./items/"
 
 # clear output directory
 if os.path.exists("./items"):
@@ -24,12 +25,12 @@ else:
 items = KeyValue.parse(open("./src/items.txt"))['DOTAAbilities']
 
 # find all items with mana or health requirements
-for key in items:
+for item_name in items:
 
-    if type(items[key]) is not dict:
+    if type(items[item_name]) is not dict:
         continue
 
-    item = items[key]
+    item = items[item_name]
 
     if 'AbilityManaCost' not in item:
         continue
@@ -37,27 +38,30 @@ for key in items:
     cost = item['AbilityManaCost']
     color = fill_blue
 
-    # some items require 0 mana, like blink or soul ring
-    # however, there are special cases like soul ring
+    # some items require 0 mana, like 'blink dagger' or 'soul ring'
     if cost == '0':
         # ok, we look if the items sacrifice health, e.g. soul ring
         try:
-            for skey in item['AbilitySpecial']:
-                for sskey in item['AbilitySpecial'][skey]:
-                    if sskey == "health_sacrifice":
-                        cost = item['AbilitySpecial'][skey][sskey]
-                        raise Exception("Found a sacrifice, lol") # quick and dirty way to exit the loops once we find a health_sacrifice entry
+            for xv in item['AbilitySpecial'].itervalues():
+                if type(xv) is not dict:
+                    continue
+
+                if "health_sacrifice" in xv:
+                    cost = xv['health_sacrifice']
+                    raise Exception
+
+            # if manacost is 0, and item doesn't require health there is no point rendering an image for that item
             continue
         except:
             color = fill_green # sacrifice health, health is green, we use green
 
-    # handle items that are upgradable
+    # handle items that are upgradable (costs for each level is separated by whitespaces)
     else:
-        if cost.find(' ') != -1:
+        if ' ' in cost:
             costs = cost.split(' ')
 
             try:
-                idx = int(key[-1]) - 1
+                idx = int(item_name[-1]) - 1 #e.g. item_dagon, item_dagon_2, item_dagon_3
             except:
                 idx = 0
 
@@ -65,13 +69,16 @@ for key in items:
 
     # center text in our little box
     text_pos_padding = (3 - len(cost)) * 4
-    filename = key[5:]
+    filename = "%s.png" % item_name[5:] # removes 'item_' prefix
 
-    # open image
-    if os.path.exists(os.path.join(presrc_image_path, "%s.png" % filename)):
-        img = Image.open(os.path.join(presrc_image_path, "%s.png" % filename))
-    else:
-        img = Image.open(os.path.join(src_image_path, "%s.png" % filename))
+    # first we check for an available prerendered src image, and fallback to the ones from Dota
+    image_src_path = os.path.join(src_preimages_root, filename)
+
+    if not os.path.exists(image_src_path):
+        image_src_path = os.path.join(src_images_root, filename)
+
+    # open the image
+    img = Image.open(image_src_path);
 
     # add info in the bottom left corner
     d = ImageDraw.Draw(img)
@@ -80,4 +87,4 @@ for key in items:
     del d
 
     # save the image in the ouput directory
-    img.save(os.path.join(out_image_path, "%s.png" % filename))
+    img.save(os.path.join(out_image_root, filename))
