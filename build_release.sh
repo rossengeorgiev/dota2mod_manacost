c35f80f152ef28d22aaf59dc3383a92cb909fa9b
Rossen Georgiev <zoorty@gmail.com>
2014-04-16 00:50:27 +0100

    updated code for generating showcase image

--- build_release.sh
+++ build_release.sh
@@ -12,6 +12,6 @@ zip -9 release.zip ./items/*
 echo -e "\nMake showcase image.."
 
 cd ./items
-montage `ls` -geometry "+2+2" -tile 5 ../showcase.png
+montage -crop 100x64+0+0 `ls` -tile 6 -shadow -geometry "+11+2" -monitor - | convert - -monitor -crop 722x+0+0 ../showcase.png
 
 echo -e "\nFinished"
