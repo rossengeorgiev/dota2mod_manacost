c6a02f178fdca4902e98d43c72ec5f0b0362bb7e
Rossen Georgiev <zoorty@gmail.com>
2014-04-18 16:19:22 +0100

    tweaks to build_release.sh

--- build_release.sh
+++ build_release.sh
@@ -12,6 +12,8 @@ zip -9 release.zip ./items/*
 echo -e "\nMake showcase image.."
 
 cd ./items
-montage -crop 100x64+0+0 `ls` -tile 6 -shadow -geometry "+11+2" -monitor - | convert - -monitor -crop 722x+0+0 ../showcase.png
+montage -crop 88x64+0+0 `ls` -tile 6 -shadow -geometry "+16+2" -monitor - | convert - -monitor -crop 722x+0+0 ../showcase.png
+
+#montage -crop 88x64+0+0 `ls` -tile 6 -geometry "+0+0" -background black -monitor - | convert - -monitor -resize 512x512 ../showcase_512x512.jpg
 
 echo -e "\nFinished"
